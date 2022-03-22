c7836b611988130e5da827933d95a8455cfe43f8
Mica <mika89700@gmail.com>
2022-03-22 22:27:47 +0100

    upload cirq_5 && runtime sampler

---
+++ resources/scripts/circuit_5.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+#############################################
+#
+# circuit_5.py
+#
+# example code for configurable circuit 5
+# from https://arxiv.org/abs/1905.10876 fig 3.
+#
+#############################################
+
+from qiskit.circuit.library import TwoLocal
+from qiskit.utils import algorithm_globals
+
+
+def circuit_2(width=4, layer=1, shots=1024, seed1=42, verbose=False):
+    nb_qubits = width
+    shots = shots
+    repetitions = layer
+    algorithm_globals.random_seed = seed1
+    # seed2 = seed2
+
+    rotation_blocks = ['rx', 'rz']
+    entanglement_blocks = 'crz'
+
+    entangler_map = []
+    for i in range(nb_qubits):
+        for u in range(nb_qubits):
+            if u != i:
+                entangler_map.append((i, u))
+
+    entanglement = entangler_map
+    insert_barriers = True
+    skip_final_rotation_layer = False
+
+    circuit = TwoLocal(num_qubits=nb_qubits,
+                       rotation_blocks=rotation_blocks,
+                       entanglement_blocks=entanglement_blocks,
+                       entanglement=entanglement,
+                       reps=repetitions,
+                       insert_barriers=insert_barriers,
+                       skip_final_rotation_layer=skip_final_rotation_layer)
+    circuit.measure_all()
+
+    if verbose:
+        print(entangler_map)
+        print(circuit.decompose())
+
+    return circuit
---
+++ resources/scripts/run_sampler.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+#############################################
+#
+# run_sampler.py
+#
+# Runtime launcher for sampler program.
+# doc : https://cloud.ibm.com/docs/quantum-computing?topic=quantum-computing-example-sampler
+#
+#############################################
+
+from qiskit_ibm_runtime import IBMRuntimeService, IBMSampler
+import numpy as np
+
+
+def run_sampler(circuit, shots=1024, seed1=42, verbose=False):
+    circuit = circuit
+    shots = shots
+    np.random.seed(seed=seed1)
+    theta = np.random.rand(len(circuit.parameters))
+
+    service = IBMRuntimeService(auth="legacy", token="", instance="ibm-q/open/main")
+    sampler_factory = IBMSampler(service=service, backend="ibmq_qasm_simulator")
+
+    with sampler_factory(circuits=circuit) as sampler:
+        result = sampler(circuit_indices=[0], shots=shots, parameter_values=[theta])
+        if verbose:
+            print(result)
