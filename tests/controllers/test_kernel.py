3a08834f8e54706647f1e58adf383f762b083fce
Michaël <michael.rollin@protonmail.com>
2022-04-03 14:55:22 +0200

    Refacto Kernel endpoint + tests (#21)
    - [x] New functions for kernel controller
    - [x] Added tests for controllers functions
    - [x] Allow tox to get envvar 
    - [x] Added tests for authentication cli
    - [x] Added tests for kernel endpoint & workflow
    - [x] Added tests for runtime

---
+++ tests/controllers/test_kernel.py
@@ -0,0 +1,35 @@
+"""Tests for Kernel commands."""
+from unittest import TestCase
+from src.controllers import kernel_endpoint, gen_circuits_tpl, gen_kernel_circuits
+from tests.test_workflow import authentication
+
+
+class TestUtils(TestCase):
+    """Test class for kernel endpoint."""
+
+    def test_kernel_endpoint(self):
+        """Test for kernel endpoint function."""
+        authentication()
+        circuits_tpl = [5, 2]
+        feather_files = kernel_endpoint(
+            circuit_tpl_id=circuits_tpl, seed_x=[42], seed_y=[4242]
+        )
+
+        for circuit, fea_file in zip(circuits_tpl, feather_files):
+            self.assertEqual(fea_file, "kernels-" + str(circuit) + "-ideal.csv")
+
+    def test_gen_kernel_components(self):
+        """Test for gen circuits template & gen kernel circuits function."""
+        circuits_tpl_id = [5, 2]
+        circuits_tpl = gen_circuits_tpl(circuit_tpl_id=circuits_tpl_id)
+        self.assertEqual(len(circuits_tpl), len(circuits_tpl_id))
+
+        seed_x = [42, 24]
+        seed_y = [4242, 2424]
+        self.assertEqual(len(seed_x), len(seed_y))
+
+        kernel_cirq = gen_kernel_circuits(
+            circuits_tpl=circuits_tpl, seed_x=seed_x, seed_y=seed_y
+        )
+        self.assertEqual(len(kernel_cirq), len(seed_x) * len(circuits_tpl))
+        self.assertEqual(len(kernel_cirq), len(seed_y) * len(circuits_tpl))
---
+++ tests/test_workflow.py
@@ -0,0 +1,40 @@
+"""Tests for CLI commands."""
+import os
+from unittest import TestCase
+from qiskit_ibm_runtime import IBMRuntimeService
+import pandas as pd
+from src.workflow import Workflow
+
+
+def authentication():
+    """Authentication function."""
+    qs_token = os.environ.get("QS_TOKEN")
+    if qs_token is not None:
+        Workflow.authentication(auth="legacy", token=qs_token, overwrite=True)
+
+
+class TestUtils(TestCase):
+    """Test class for Workflow cli."""
+
+    def test_authentication(self):
+        """Test for Authentication command."""
+        authentication()
+
+        service = IBMRuntimeService()
+        self.assertTrue(service.active_account()["verify"], True)
+
+    def test_kernel_flow(self):
+        """Test for Kernel flow command."""
+        authentication()
+        circuits_tpl = [5]
+        feather_files = Workflow.kernel_flow(
+            circuit_tpl_id=circuits_tpl, matrix_size=[1, 1]
+        )
+
+        for circuit, fea_file in zip(circuits_tpl, feather_files):
+            self.assertEqual(fea_file, "kernels-" + str(circuit) + "-ideal.csv")
+
+    def test_view_kernel(self):
+        """Test to test the view kernel command."""
+        data_file = Workflow.view_kernel(file_name="kernels-2-ideal.csv")
+        self.assertTrue(isinstance(data_file, pd.DataFrame))
