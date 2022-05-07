41abbfa2f87faa833a6ca8265c5676745c74af6b
Mica <mika89700@gmail.com>
2022-05-07 14:59:19 +0200

    add docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd /opt/experiments/q-kernel-ops/resources/kernel_metadata
+
+python workflow.py authentication --channel="ibm_quantum" --token="${QS_TOKEN}" --instance="ibm-q/open/main"
+
 ./gen_data.sh
+
