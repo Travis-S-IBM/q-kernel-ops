cacb1514f72328e34d8f9f489aeb145799ae6bdf
Mica <mika89700@gmail.com>
2022-05-10 23:56:12 +0200

    delete docker stuff

--- docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /opt/experiments/q-kernel-ops/resources/kernel_metadata
-
-python workflow.py authentication --channel="ibm_quantum" --token="${QS_TOKEN}" --instance="ibm-q/open/main"
-
-./gen_data.sh
-
