e9af484b59223033a567dd022350854d6e6d4720
cfillekes <cfilleke@redhat.com>
2022-06-09 11:37:44 -0400

    Update kernel-run.sh
    have kernel-run use all externally defined environment variables

--- image/kernel-run.sh
+++ image/kernel-run.sh
@@ -5,23 +5,18 @@
 # 0. Log in to your IBM Quantum Services account
 #    go to your account profile & copy your auth token 
 #
-# 1. put your Quantum Services token in an enviroment
-#    variable called QS_TOKEN:
-#
-#  $ export QS_TOKEN="your-big-old-Quantum-services-token"
+# 1. configure default-env file as described in README.md
 #
 # 2. now run this script
 #
 
-export REPO=q-kernel-ops
-export DATA=resources/kernel_metadata
 
 podman run -d -v ${HOME}/${REPO}/${DATA}:/${DATA} \
        -e QS_TOKEN=${QS_TOKEN}                    \
        -e BACKEND="ibmq_qasm_simulator"           \
-       -e MATRIX_SIZE=64                          \
-       -e CIRCUIT_ID=2                            \
-       -e LAYER=2                                 \
-       quay.io/qiskit/qmlrun:0.0.3-z 
+       -e MATRIX_SIZE=${MATRIX_SIZE}              \
+       -e CIRCUIT_ID=${CIRGUIT_ID}                \
+       -e LAYER=${LAYER}                          \
+       quay.io/qiskit/qmlrun:0.0.4-z 
 
 
